fe704e266a3f16ea36769fd7d62a99218e00d231
Tao Wen <taowen@chengfayun.com>
2021-03-18 15:44:05 +0800

    adapt mysql query

--- src/MysqlDatabase.ts
+++ src/MysqlDatabase.ts
@@ -1,17 +1,57 @@
-import { Database, Scene, Table } from '@rotcare/io';
+import { Database, Scene, Table, uuid } from '@rotcare/io';
+import * as mysql from 'mysql2/promise';
 
 export class MysqlDatabase implements Database {
-    insert(scene: Scene, table: Table<any>, props: Record<string, any>): Promise<any> {
-        throw new Error('Method not implemented.');
+    constructor(private readonly conn: mysql.Connection) {}
+    public async insert(scene: Scene, table: Table<any>, props: Record<string, any>): Promise<any> {
+        if (!props.id) {
+            props.id = uuid();
+        }
+        const columns = Object.keys(props);
+        const placeholders = ',?'.repeat(columns.length).substr(1);
+        await this.conn.execute(
+            `INSERT INTO ${table.tableName} (${columns.join(', ')}) VALUES(${placeholders});`,
+            columns.map((col) => props[col]),
+        );
+        return table.create(props);
     }
-    query(scene: Scene, table: Table<any>, props: Record<string, any>): Promise<any[]> {
-        throw new Error('Method not implemented.');
+    public async update(
+        scene: Scene,
+        table: Table<any>,
+        props: Record<string, any>,
+    ): Promise<void> {
+        const columns = Object.keys(props);
+        await this.conn.execute(
+            `UPDATE ${table.tableName} SET ${columns.map((col) => `${col}=?`).join(', ')}`,
+            columns.map((col) => props[col]),
+        );
     }
-    update(scene: Scene, table: Table<any>, props: Record<string, any>): Promise<void> {
-        throw new Error('Method not implemented.');
+    public async query(
+        scene: Scene,
+        table: Table<any>,
+        props: Record<string, any>,
+    ): Promise<any[]> {
+        const columns = Object.keys(props);
+        let sql = `SELECT * FROM ${table.tableName}`;
+        if (columns.length > 0) {
+            sql = `${sql} WHERE ${columns.map((col) => `${col}=?`).join(' AND ')}`;
+        }
+        const [rows] = await this.conn.execute(
+            sql,
+            columns.map((col) => props[col]),
+        );
+        return (rows as any[]).map((row) => table.create(row));
     }
-    delete(scene: Scene, table: Table<any>, props: Record<string, any>): Promise<void> {
-        throw new Error('Method not implemented.');
+    public async delete(
+        scene: Scene,
+        table: Table<any>,
+        props: Record<string, any>,
+    ): Promise<void> {
+        if (!props.id) {
+            throw new Error('expect id');
+        }
+        debugger;
+        await this.conn.execute(`DELETE FROM ${table.tableName} WHERE id=?`, [props.id]);
     }
     executeSql(scene: Scene, sql: string, sqlVars: Record<string, any>): Promise<any[]> {
         throw new Error('Method not implemented.');
--- src/MysqlDatabase_test.ts
+++ src/MysqlDatabase_test.ts
@@ -1,18 +1,52 @@
+import { Entity, newTrace, Scene } from '@rotcare/io';
 import * as mysql from 'mysql2/promise';
+import { MysqlDatabase } from './MysqlDatabase';
+import { strict } from 'assert';
 
 describe('MysqlDatabase', () => {
+    let conn: mysql.Connection;
+    beforeEach(async () => {
+        conn = await mysql.createConnection(require('./testConn'));
+        await conn.execute('DROP TABLE IF EXISTS Product')
+        await conn.execute(`CREATE TABLE Product (
+            id varchar(255) PRIMARY KEY,
+            name varchar(255),
+            price int)`)
+    })
+    afterEach(async () => {
+        await conn.end();
+    });
     it('增删改查', async () => {
-        var connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'test'
+        class Product extends Entity {
+            id: string;
+            name: string;
+            price: number = 0;
+            public static async createProduct(scene: Scene, props: Partial<Product>) {
+                return await scene.io.database.insert(scene, Product, props);
+            }
+            public static async queryProduct(scene: Scene, props: Partial<Product>) {
+                return await scene.io.database.query(scene, Product, props);
+            }
+            public async updatePrice(scene: Scene, newPrice: number) {
+                this.price = newPrice;
+                await scene.io.database.update(scene, this.table, this);
+            }
+            public async deleteMe(scene: Scene) {
+                await scene.io.database.delete(scene, this.table, this);
+            }
+        }
+        const database = new MysqlDatabase(conn);
+        const scene = new Scene(newTrace('test'), {
+            database,
+            serviceProtocol: undefined as any
         });
-
-        await connection.connect();
-
-        const [result] = await connection.query('SELECT 1 + 1 AS solution');
-        console.log((result as any)[0].solution);
-        await connection.end();
+        await scene.execute(undefined, async() => {
+            const apple = await scene.create(Product, { name: 'apple' });
+            strict.ok(apple.id);
+            await apple.updatePrice(scene, 100);
+            strict.equal((await scene.query(Product, { price: 100 })).length, 1);
+            await apple.deleteMe(scene);
+            strict.equal((await scene.query(Product, {})).length, 0);
+        })
     });
 });
