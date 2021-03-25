b8f72788d192c0016d62cf157b35a9a0fe8f6db8
Tao Wen <taowen@chengfayun.com>
2021-03-25 23:07:55 +0800

    add back test

---
+++ src/MysqlDatabase_test.ts
@@ -0,0 +1,68 @@
+import { Entity, Scene } from '@rotcare/io';
+import { strict } from 'assert';
+import { should } from './should';
+import { subsetOf } from './subsetOf';
+
+export class Product extends Entity {
+    public id: string;
+    public name: string;
+    public price: number = 0;
+
+    public static cheapProducts = subsetOf(Product, 'price < 100', class {});
+
+    public static queryProductsByPriceRange = subsetOf(
+        Product,
+        'price > :lowPrice AND price < :highPrice',
+        class {
+            highPrice: number;
+            lowPrice: number;
+        },
+    );
+
+    public static async createProduct(scene: Scene, props: Partial<Product>) {
+        return await scene.io.database.insert(scene, Product, props);
+    }
+    public static async queryProduct(scene: Scene, props: Partial<Product>) {
+        return await scene.io.database.query(scene, Product, props);
+    }
+    public async updatePrice(scene: Scene, newPrice: number) {
+        this.price = newPrice;
+        await scene.io.database.update(scene, this.table, this);
+    }
+    public async deleteMe(scene: Scene) {
+        await scene.io.database.delete(scene, this.table, this);
+    }
+}
+
+async function recreateProductTable(scene: Scene) {
+    await scene.io.database.executeSql(scene, 'DROP TABLE IF EXISTS Product', {});
+    await scene.io.database.executeSql(scene, `CREATE TABLE Product (
+        id varchar(255) PRIMARY KEY,
+        name varchar(255),
+        price int)`, {});
+}
+
+describe('MysqlDatabase', () => {
+    it(
+        '增删改查',
+        should('work', async (scene) => {
+            await recreateProductTable(scene);
+            const apple = await scene.create(Product, { name: 'apple' });
+            strict.ok(apple.id);
+            await apple.updatePrice(scene, 100);
+            strict.equal((await scene.query(Product, { price: 100 })).length, 1);
+            await apple.deleteMe(scene);
+            strict.equal((await scene.query(Product, {})).length, 0);
+        }),
+    );
+    xit(
+        'query cheapsProducts',
+        should('return a subset of data', async (scene) => {
+            await recreateProductTable(scene);
+            await scene.create(Product, { name: 'apple', price: 102 });
+            await scene.create(Product, { name: 'pear', price: 99 });
+            const cheapProducts = await scene.query(Product.cheapProducts, {});
+            strict.equal(1, cheapProducts.length);
+        }),
+    );
+});
